c020a608003c8d470130c48a2e4659342e03e7cc
biginfo2012 <biginfo2012@gmail.com>
2024-04-02 13:20:18 +0530

    added progress bar progression functionality

--- src/components/progressCard/progressBar.tsx
+++ src/components/progressCard/progressBar.tsx
@@ -5,15 +5,21 @@ import { PersonalInfoContext } from "../../web3/PersonalInfo";
 
 export default function ProgressBar() {
   const { poolState } = useContext(PersonalInfoContext);
+  const percentage = (poolState.raised / poolState.hardcap) * 100;
   return (
     <div className={classes.progress__bar__wrapper}>
       <div className={classes.progress__bar__text__wrapper}>
-        <p className={classes.progress__bar__text}>{poolState.raised} / {poolState.hardcap} SOL</p>
-        <p className={classes.progress__bar__text}>Progress {poolState.raised / poolState.hardcap * 100}%</p>
+        <p className={classes.progress__bar__text}>
+          {poolState.raised} / {poolState.hardcap} SOL
+        </p>
+        <p className={classes.progress__bar__text}>Progress {percentage}%</p>
       </div>
       <div className={classes.progress__bar__container}>
         <div className={classes.progress__bar}>
-          <div className={classes.progress__bar__inner}></div>
+          <div
+            className={classes.progress__bar__inner}
+            style={{ width: `${percentage}%` }}
+          ></div>
         </div>
       </div>
     </div>
